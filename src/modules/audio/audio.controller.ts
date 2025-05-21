866202238fa15f915b7a13781e3ac27ee8dfa0a1
Rafael Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2025-05-21 16:45:24 -0300

    test

--- src/modules/audio/audio.controller.ts
+++ src/modules/audio/audio.controller.ts
@@ -51,12 +51,6 @@ export class AudioController {
         filter: 'audioonly',
         quality: 'highestaudio',
         agent,
-        requestOptions: {
-          headers: {
-            'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-          },
-        },
       });
 
       audioStream.on('error', (err) => {
