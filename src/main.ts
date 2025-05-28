cd9ca304151925751b646f78604dbd45f26e6bfd
Rafael Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2025-05-28 00:57:27 -0300

    feat: morgan

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
+import morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configure Morgan for HTTP request logging
+  app.use(morgan('dev')); // Logs: :method :url :status :response-time ms
 
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
